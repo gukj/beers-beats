38c3cf7f9e1a1dc1627a44f0b29b6000309d3e99
juliavh <juliavh@kth.se>
2017-03-22 14:01:17 +0100

    model first draft

---
+++ app/js/model.js
@@ -0,0 +1,30 @@
+//Get data from spoitfy and beer API respectively
+
+beersBeatsApp.Factory('model', function($resource, $cookieStore){
+
+	/* API CALLS */
+
+	//get beers given a country
+
+	//get country given a beer
+
+	//get playlist given country
+
+	//get country given playlist
+
+
+	/* MODEL */
+
+	//given a beer, get its country and retun playlist
+
+	//given a playlist, get its country and return list of beers
+
+	//add beers to BAG/favourites
+
+	//add playlists to favourites
+
+	//add beer-playlist combo to playlist
+
+
+	return this;
+});
