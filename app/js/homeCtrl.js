46717d85d48d2dd340ebd576d4e142909a1e52fe
Julia Sporre <juliasp@kth.se>
2017-03-23 12:41:42 +0100

    added description in model

--- app/js/homeCtrl.js
+++ app/js/homeCtrl.js
@@ -2,10 +2,9 @@ beersBeatsApp.controller('homeCtrl', function($scope, model){
 
 
   /*var name = 'MasterMind';
-  model.BeerCategory.get({name:name},function(data){
+  model.BeerByName.get({name:name},function(data){
   console.log(data);
   $scope.beer = data;
-
   $scope.status = "Showing " + data + " results";
     },function(data){
       $scope.status = "There was an error";
--- app/js/model.js
+++ app/js/model.js
@@ -2,39 +2,78 @@
 
 beersBeatsApp.factory('model', function($resource, $cookieStore){
 
+	var _this = this;
+
 	/* API CALLS */
 
 	//get beer by name
-	// input name of beer
+	// input name of bee
+	/* In ctrl:
+	var name = blabla;
+	model.BeerByName.get({name:name},function(data){
+	$scope.beer = data.data;
+	$scope.status = "Showing " + data + " results";
+		},function(data){
+			$scope.status = "There was an error";
+	});
+	*/
+
+
 	this.BeerByName = $resource('http://api.brewerydb.com/v2/search?q=:name&type=beer&key=81f290d3c2a50e872349732640d52269',{},{
     get: {
+			headers: {},
       success: function(data) {
-        cb(data.data);
-      },
-      error: function(data) {
-    }
+        cb(data);
+      }
     }
   });
 
 	// get beer by ID
 	//input id
+	/* In ctrl:
+	var name = blabla;
+	model.BeerByID.get({name:name},function(data){
+	$scope.beer = data.data;
+	$scope.status = "Showing " + data + " results";
+		},function(data){
+			$scope.status = "There was an error";
+	});
+	*/
+
+
 	this.BeerByID = $resource('http://api.brewerydb.com/v2/beer/:id/?key=81f290d3c2a50e872349732640d52269',{},{
     get: {
+			headers: {},
       success: function(data) {
-        cb(data.data);
+        cb(data);
       },
       error: function(data) {
+				cb('error');
     }
     }
   });
 
 	//get random beer
+
+	/* In ctrl:
+	var name = blabla;
+	model.RandomBeer.get({name:name},function(data){
+	$scope.beer = data.data;
+	$scope.status = "Showing " + data + " results";
+		},function(data){
+			$scope.status = "There was an error";
+	});
+	*/
+
+
 	this.RandomBeer = $resource('http://api.brewerydb.com/v2/beer/random/?key=81f290d3c2a50e872349732640d52269',{},{
     get: {
+			headers: {},
       success: function(data) {
         cb(data.data);
       },
       error: function(data) {
+				cb('error');
     }
     }
   });
@@ -43,12 +82,24 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
 
 	//get country (category) given a beer
 	//input name of beer
+
+	/* In ctrl:
+	var name = blabla;
+	model.BeerCategory.get({name:name},function(data){
+	$scope.beer = data.data[0].style.categoryId;
+	$scope.status = "Showing " + data + " results";
+		},function(data){
+			$scope.status = "There was an error";
+	});
+	*/
 	this.BeerCategory = $resource('http://api.brewerydb.com/v2/search?q=:name&type=beer&key=81f290d3c2a50e872349732640d52269',{},{
 		get: {
+			headers: {},
 			success: function(data) {
 				cb(data.data[0].style.categoryId)
 			},
 			error: function(data) {
+				cb('error');
 		}
 		}
 	});
