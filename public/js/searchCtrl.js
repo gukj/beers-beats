a5d20df3b82243c317d235d1aa56d62054732d7c
Julia Sporre <juliasp@kth.se>
2017-03-29 19:48:32 +0200

    fixes in search. Now directing to selectedBeer

--- public/js/searchCtrl.js
+++ public/js/searchCtrl.js
@@ -2,24 +2,26 @@ beersBeatsApp.controller('searchCtrl', function($scope, model){
 
 
   $scope.search = function(name) {
-  $scope.status = "Searching...";
-  model.BeerByName.get({name:name},function(data){
-    $scope.beers = data;
-    $scope.status = "Showing " + data.length + " results";
-    },function(data){
-      $scope.status = "There was an error";
-    });
+
+    if(name == 'rnd'){
+      $scope.status = "Looking for the perfect random beer...";
+      model.RandomBeer.get({},function(data){
+        $scope.beers = {data};
+        $scope.status = "";
+        },function(data){
+          $scope.status = "There was an error";
+      });
+    } else {
+      $scope.status = "Searching for some nice beers...";
+      model.BeerByName.get({name:name},function(data){
+        $scope.beers = data;
+        $scope.status = "Showing " + data.length + " results";
+        },function(data){
+          $scope.status = "There was an error";
+      });
+    }
   }
 
-  $scope.searchRandom = function() {
-  $scope.status = "Searching...";
-  model.RandomBeer.get({},function(data){
-    $scope.randomBeer = data;
-    $scope.status = "";
-    },function(data){
-      $scope.status = "There was an error";
-    });
-  }
 
   $scope.addBeer = function(beerID) {
     model.selectBeer(beerID);
