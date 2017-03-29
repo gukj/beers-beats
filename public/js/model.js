cade84804a4887b2dcdc7e8de89dc5fa7aa4872a
juliavh <juliavh@kth.se>
2017-03-29 14:21:57 +0200

    layout fix and remove functionality

--- public/js/model.js
+++ public/js/model.js
@@ -6,16 +6,47 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
 
 	var selectedBeers = [];
 
+	/*var cat1 = '0tkRbaSwTy9lwAw66vCCIq'; // British Origin Ales
+	var cat2 = '2xqLn5C8UBdG63mmy4i8QQ'; // Irish Origin Ales
+	var cat3 = '2OfjDjNGlp8Z9uh2yNCzkb' // North American Origin Ales
+	var cat4 = '1G4xK6ksyXGLqkf4QSSQCL'; // German Origin Ales
+	var cat5 = '1Bx9uZMuiLZ6ZT8N1fmEjp'; // Belgian And French Origin Ales
+	var cat6 = '37i9dQZEVXbMDoHDwVN2tF'; // International Ale Styles
+	var cat7 = '1uCcS1S6u16qk8JXKgfIqS'; // European-germanic Lager
+	var cat8 = '2fDLCkAiNIeQzXbdG86g9r'; // North American Lager
+	var cat9 = '3oaoPa6YMNdfx4yc05aVmt'; // Other Lager
+	var cat10 = '0jXuFjs93Tavfsy19s1lk6'; // International Style
+	var cat11 = '37i9dQZF1DX0sQWfevMRw3'; // Hybrid/mixed Beer
+	var cat12 = '6qIrNhoRraUIWhsktGP5bj'; // Mead, Cider, & Perry
+	var cat13 = '72OAW0y5ntDYCURQlCwKHl'; //Other Origin
+	var cat14 = '7ilySkaswwP6MeaboU010g'; // Malternative Beverages*/
+
+	var playlistIDs = ['0tkRbaSwTy9lwAw66vCCIq', // British Origin Ales
+										'2xqLn5C8UBdG63mmy4i8QQ', // Irish Origin Ales
+										'2OfjDjNGlp8Z9uh2yNCzkb', // North American Origin Ales
+										'1G4xK6ksyXGLqkf4QSSQCL', // German Origin Ales
+										'1Bx9uZMuiLZ6ZT8N1fmEjp', // Belgian And French Origin Ales
+										'37i9dQZEVXbMDoHDwVN2tF', // International Ale Styles
+										'1uCcS1S6u16qk8JXKgfIqS', // European-germanic Lager
+										'2fDLCkAiNIeQzXbdG86g9r', // North American Lager
+										'3oaoPa6YMNdfx4yc05aVmt', // Other Lager
+										'0jXuFjs93Tavfsy19s1lk6', // International Style
+										'37i9dQZF1DX0sQWfevMRw3', // Hybrid/mixed Beer
+										'6qIrNhoRraUIWhsktGP5bj', // Mead, Cider, & Perry
+										'72OAW0y5ntDYCURQlCwKHl', //Other Origin
+										'7ilySkaswwP6MeaboU010g' // Malternative Beverages
+										];
 	/* API CALLS */
 
 //get beer by name
 //input name of beer
-	this.BeerByName = $resource('http://api.brewerydb.com/v2/search?q=:name&type=beer&key=81f290d3c2a50e872349732640d52269',{},{
+	this.BeerByName = $resource('https://crossorigin.me/http://api.brewerydb.com/v2/search?q=:name&type=beer&key=81f290d3c2a50e872349732640d52269',{},{
     get: {
 			method: 'GET',
 			isArray: true,
 			transformResponse: function(data){
 				var tmp =  angular.fromJson(data);
+				console.log(tmp.data);
 				return tmp.data ;
 			}
 	  }
@@ -23,7 +54,7 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
 
 //get beer by id
 //input id of beer
-	this.BeerByID = $resource('http://api.brewerydb.com/v2/beer/:id/?key=81f290d3c2a50e872349732640d52269',{},{
+	this.BeerByID = $resource('https://crossorigin.me/http://api.brewerydb.com/v2/beer/:id/?key=81f290d3c2a50e872349732640d52269',{},{
     get: {
 			method: 'GET',
 			transformResponse: function(data){
@@ -34,12 +65,12 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
   });
 
 	//get random beer
-	this.RandomBeer = $resource('http://api.brewerydb.com/v2/beer/random/?key=81f290d3c2a50e872349732640d52269',{},{
+	this.RandomBeer = $resource('https://crossorigin.me/http://api.brewerydb.com/v2/beer/random/?key=81f290d3c2a50e872349732640d52269',{},{
     get: {
 			method: 'GET',
 			transformResponse: function(data){
 				var tmp =  angular.fromJson(data);
-				console.log(tmp.data);
+				console.log(tmp);
 				return tmp.data;
 			}
     }
@@ -49,7 +80,7 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
 
 	//get country (category) given a beer
 	//input name of beer
-	this.BeerCategory = $resource('http://api.brewerydb.com/v2/search?q=:name&type=beer&key=81f290d3c2a50e872349732640d52269',{},{
+	this.BeerCategory = $resource('https://crossorigin.me/http://api.brewerydb.com/v2/search?q=:name&type=beer&key=81f290d3c2a50e872349732640d52269',{},{
 		get: {
 			method: 'GET',
 			transformResponse: function(data){
@@ -61,12 +92,10 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
 		}
 	});
 
-	//TODO get image of a beer
 
-
-	//get playlist given country
-
-	//get country given playlist
+	//@JULIAVONHEIJNE: TODO get image of a beer: Ska ligga hos controllern. Använd 
+	//this.beerbyID to access the whole j-son object and then get 
+	//the specific json object for image url.
 
 
 	/* MODEL */
@@ -75,6 +104,10 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
 
 	//given a playlist, get its country and return list of beers
 
+	//get playlist given country
+
+	//get country given playlist
+
 	//add beer object to BAG/favourites
 	this.selectBeer = function(beer){
 		selectedBeers.push(beer);
@@ -82,6 +115,7 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
 
 	//remove beers to BAG/favourites
 	this.deselectBeer = function(beerID){
+
 		for (var i=0; i<selectedBeers.length; i++){
 			if(selectedBeers[i].id === beerID){
 				selectedBeers.splice(i,1);
@@ -98,6 +132,38 @@ beersBeatsApp.factory('model', function($resource, $cookieStore){
 	//add playlists to favourites
 
 	//add beer-playlist combo to playlist
+
+	this.generatePlaylist = function(){
+		var categories = {
+			1 : 0,
+			2 : 0,
+			3 : 0,
+			4 : 0,
+			5 : 0,
+			6 : 0,
+			7 : 0,
+			8 : 0,
+			9 : 0,
+			10 :0,
+			11 : 0,
+			12 : 0,
+			13 : 0,
+			14 : 0
+		};
+		for (var i = 1; i < selectedBeers.length; i++) {
+    	categories[selectedBeers[i].categoryId] = categories[selectedBeers[i].categoryId] + 1;
+		}
+		console.log(categories);
+		var highestValue = -1;
+		var selectedID = '';
+		for (i = 1; i < categories.length; i++) {
+			if(categories[i] > highestValue){
+				selectedID = i;
+				highestValue = categories[i];
+			}
+		}
+		return playlistIDs[selectedID-1];
+	}
 
 
 	return this;
