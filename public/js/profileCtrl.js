e09c62d8a6998cd372746c0c9148d56d2cdfd553
juliavh <juliavh@kth.se>
2017-04-07 12:58:11 +0200

    profile image added

--- public/js/profileCtrl.js
+++ public/js/profileCtrl.js
@@ -2,6 +2,7 @@ beersBeatsApp.controller('profileCtrl', function($scope, model){
 
   this.getProfile = model.userProfile.get({},function(data){
     $scope.profile = data;
+    console.log(data);
   });
 
   this.getProfile;
