65742ade14f5ffd46e9a1eedc946dabc39bff238
juliavh <juliavh@kth.se>
2017-04-10 10:24:49 +0200

    bug fixed

--- public/js/beerCtrl.js
+++ public/js/beerCtrl.js
@@ -6,10 +6,12 @@ beersBeatsApp.controller('beerCtrl', function($scope, model){
 	}
 
 	$scope.toggleText = function(id){
+		console.log('toggle id', id);
 		model.toggleBeerText(id);
 	}
 
 	$scope.remove = function(id){
+		console.log('model id', id);
 		model.deselectBeer(id);
 	}
 });
