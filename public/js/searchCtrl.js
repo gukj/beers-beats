14fd30670ead569db77c67c1ef8abf0368898af7
juliasporre <juliasp@kth.se>
2017-04-10 11:07:50 +0200

    cancel-button now working in loading bar

--- public/js/searchCtrl.js
+++ public/js/searchCtrl.js
@@ -1,16 +1,22 @@
 beersBeatsApp.controller('searchCtrl', function($scope, model){
 
   var _this = this;
+  _this.cModal = false;
 
+
+  //Gets the data from API request
   $scope.search = function(name) {
-
+    $scope.results = "";
+    _this.cModal = false;
+    //If you search for a random beer, the name == rnd from scope
     if(name == 'rnd'){
-      $scope.status = "Looking for the perfect random beer...";
+      $scope.status = "Searching for the perfect random beer...";
       _this.openModal();
       model.RandomBeer.get({},function(data){
-        $scope.beers = {data};
-        $scope.status = "";
-        _this.closeModal();
+        if (_this.cModal == false){
+          $scope.beers = {data};
+          _this.closeModal();
+        }
         },function(data){
           $scope.status = "There was an error";
       });
@@ -18,31 +24,36 @@ beersBeatsApp.controller('searchCtrl', function($scope, model){
       $scope.status = "Searching for some nice beers...";
       _this.openModal();
       model.BeerByName.get({name:name},function(data){
-        $scope.beers = data;
-        $scope.status = "Showing " + data.length + " results";
-        _this.closeModal();
+        if (_this.cModal == false) {
+          $scope.beers = data;
+          $scope.results = "Showing " + data.length + " results";
+          _this.closeModal();
+        }
         },function(data){
           $scope.status = "There was an error";
       });
     }
   }
 
+  //Adds beer to beer bag in model
   $scope.addBeer = function(beerID) {
     model.selectBeer(beerID);
   }
 
-
+  //Spans the modal
   this.openModal = function(message){
     $scope.checked=true;
   }
 
+  //Closes the modal
   this.closeModal = function() {
     angular.element('.modal').triggerHandler('click');
     //$scope.checked=false;
-};
+  }
 
+  //Sets CModal to true, which stops the data from the API request not being printed out
   $scope.cancelAPICall = function(){
-    console.log("...");
+    _this.cModal = true;
   }
 
 
