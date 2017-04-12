f0836f44979077e2febda8152136d168152f5652
Emelie Wängborg <emeliewangborg@Emelies-MacBook-Pro.local>
2017-04-12 15:43:53 +0200

    error feedback to user, not working yet

--- public/js/selectedBeerCtrl.js
+++ public/js/selectedBeerCtrl.js
@@ -4,10 +4,19 @@ beersBeatsApp.controller('selectedBeerCtrl', function($scope, model, $routeParam
 
 	model.BeerByID.get({id:id},function(data){
 		$scope.beer = data
+		$scope.status = "";
+	},function(data){
+		$scope.status = "There was an error, please try again.";
+		_this.openError();
 	});
 
 	$scope.add = function(id){
 		model.selectBeer(id);
 	}
+
+	//Opens error message on screen
+  	_this.openError = function(){
+    	angular.element('#errorModal').modal('show');
+  }
 
 });
