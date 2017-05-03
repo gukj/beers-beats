d8712ce67aef997846327b1b42ed5226f888981d
juliasporre <juliasp@kth.se>
2017-05-03 12:02:39 +0200

    Fixed bug in localtorage. also added a miniplayer (not working atm in other views)

--- public/js/spotifyPlaylistCtrl.js
+++ public/js/spotifyPlaylistCtrl.js
@@ -6,13 +6,17 @@ beersBeatsApp.controller('spotifyPlaylistCtrl', function($scope, model, $sce){
   if (pID == undefined){ //If we do not have any beers selected in beerBag
     $scope.spotifyModule = "";
     var message = "Hey, your Beerbag is empty! You can't listen to music without ze beer!" + '<br><br> <a href="#!/search/" class="btn btn-sm btn-primary">Go and get zum beers!</a>';
-
+    $scope.status = $sce.trustAsHtml(message);
 
   } else {
     var message = '<div>Here is your generated beerified playlist, only for you! Enjoy!</div>';
+    console.log(pID);
+    console.log(creator);
     $scope.status = $sce.trustAsHtml(message);
-    var spotifyURL = '<iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3A' + creator + '%3Aplaylist%3A' + pID + '&theme=white" width="1000" height="550" frameborder="0" allowtransparency="true"></iframe>';
-    $scope.spotifyModule = $sce.trustAsHtml(spotifyURL);
+    var spotifyPlayerURL = '<iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3A' + creator + '%3Aplaylist%3A' + pID + '&theme=white" width="1000" height="550" frameborder="0" allowtransparency="true"></iframe>';
+    var spotifyMiniPlayerURL = '<iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3A' + creator + '%3Aplaylist%3A' + pID + '&theme=white&view=coverart" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>';
+    $scope.spotifyModule = $sce.trustAsHtml(spotifyPlayerURL);
+    $scope.spotifyMiniModule = $sce.trustAsHtml(spotifyMiniPlayerURL);
   }
 
 
