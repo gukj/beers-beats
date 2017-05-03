c096198288272e6d8e3c6c91f15106eccd5afcf9
emeliewangborg <emeliewangborg@gmail.com>
2017-05-03 12:52:15 +0200

    added some info and button to beerView

--- public/js/beerCtrl.js
+++ public/js/beerCtrl.js
@@ -1,4 +1,14 @@
-beersBeatsApp.controller('beerCtrl', function($scope, model){
+beersBeatsApp.controller('beerCtrl', function($scope, model,  $sce){
+
+  	var pID = model.getPlaylistID();
+
+  	if (pID == undefined){ //If we do not have any beers selected in beerBag
+    	var message = "Hey, your Beerbag is empty! You can't listen to music without ze beer!" + '<br><br> <a href="#!/search/" class="btn btn-sm btn-primary">Go and get zum beers!</a>';
+    	$scope.status = $sce.trustAsHtml(message);
+  	}
+  	else {
+  		$scope.status = ""
+  	}
 
 	$scope.beers = function(){
 		return model.getSelectedBeers();
